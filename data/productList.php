c71071e762be3ac0a6c5b8c1558b6d8e5835f364
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-09 10:12:46 +0100

    marca

--- data/productList.php
+++ data/productList.php
@@ -8,7 +8,7 @@ $prodotti = [
     new ProductTech("tech", "Mi Band 6", "40 €", "Xiaomi", "Smartwatch", "24 mesi"),
     new ProductClothes("clothes", "Stivali Alti", "120 €", "Oxe", "Scarpe", "37"),
     new ProductClothes("clothes", "Scarpe da corsa", "100 €", "Nike", "Scarpe", "44"),
-    new ProductClothes("clothes", "T-Shirt maniche corte", "120 €", "Oxe", "Abbigliamento", "M"),
+    new ProductClothes("clothes", "T-Shirt maniche corte", "120 €", "Nike", "Abbigliamento", "M"),
     new ProductClothes("clothes", "Felpa con cappuccio", "80 €", "Adidas", "Abbigliamento", "L"),
     new ProductTech("tech", "HP Envy 12", "1200 €", "HP", "Laptop", "24 mesi"),
     new ProductTech("tech", "PowerBank", "50 €", "INIU", "PowerBank", "12 mesi"),
